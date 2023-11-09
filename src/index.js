b3925421206e21324aa3fb0b1bebe157c293d9e0
DarioFunes89 <dariofunes89@gmail.com>
2023-11-09 15:41:29 -0300

    middlewares json and urlencoded

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.listen(()=>{
-    console.log("Server is running in port 3000")
+// Middlewares
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({extended:false}));
+
+// Routes
+app.use(require('./routes/index'));
+
+app.listen((PORT)=>{
+    console.log("Server is running in port 3000 ")
 })
