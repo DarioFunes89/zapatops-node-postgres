9425c714793e57a8bcd03986966a9e2525e853e4
DarioFunes89 <dariofunes89@gmail.com>
2023-11-22 16:34:45 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getZapatillas, createZapatillas} = require('../controllers/index.controllers')
+const {getZapatillas, createZapatillas, getZapatillasById} = require('../controllers/index.controllers')
 
-router.get('/zapatillas', getZapatillas)
-
-router.post('/zapatillas', createZapatillas)
+router.get('/zapatillas', getZapatillas);
+router.get('/zapatillas/:id', getZapatillasById);
+router.post('/zapatillas', createZapatillas);
 
 module.exports = router;
