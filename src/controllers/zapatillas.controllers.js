dc4875097ced311b3f38f56f5240b063510c9337
DarioFunes89 <dariofunes89@gmail.com>
2023-11-23 15:22:49 -0300

    Update zapatillas.controllers.js

--- src/controllers/zapatillas.controllers.js
+++ src/controllers/zapatillas.controllers.js
@@ -8,10 +8,10 @@ const pool = new Pool({
     port: '5432'
 });
 
-const getZapatillas = async (req,res) =>{
+const getZapatillas = async (req,res) => {
     const response = await pool.query('SELECT * FROM zapatillas');
     res.status(200).json(response.rows);
-}
+};
 
 const getZapatillasById = async (req,res) => {
     const {id} = req.params;
@@ -28,7 +28,7 @@ const getZapatillasById = async (req,res) => {
     })}
 }
 
-const createZapatillas = async (req,res) =>{
+const createZapatillas = async (req,res) => {
     const {name, talle} = req.body;
     
     const response = await pool.query('INSERT INTO zapatillas (name, talle) VALUES ($1, $2)', [name,talle]);
@@ -38,7 +38,25 @@ const createZapatillas = async (req,res) =>{
     });
 };
 
-const deleteZapatillas = async (req,res) =>{
+const updateZapatillas = async (req, res) => {
+    const {id} = req.params;
+    const {name, talle} = req.body;
+
+    const response = await pool.query('SELECT * FROM zapatillas WHERE id = $1', [id]);
+        if(response.rows.length === 0) {
+            res.status(404).json({
+                message: 'Zapatilla inexistente'
+            })
+        } else {
+            const updateZapatilla = await pool.query('UPDATE zapatillas SET name = $1, talle = $2 WHERE id = $3', [name,talle,id]);
+            res.json({
+                message: 'Zapatilla actualizada',
+                body: {name, talle}
+            })
+        }
+};
+
+const deleteZapatillas = async (req,res) => {
     const {id} = req.params;
 
     const response = await pool.query('SELECT * FROM zapatillas WHERE id = $1', [id]);
@@ -51,14 +69,15 @@ const deleteZapatillas = async (req,res) =>{
         res.status(200).json({
             message: 'Zapatilla eliminada',
             body: responseDelete.rows[0]
-    })}
+    })};
 
-}
+};
 
 
 module.exports = {
     getZapatillas,
     createZapatillas,
     getZapatillasById,
-    deleteZapatillas
+    deleteZapatillas,
+    updateZapatillas
 }
