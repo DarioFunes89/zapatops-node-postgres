d2219181bfabeb82c2bcd487077b08893adc58f8
DarioFunes89 <dariofunes89@gmail.com>
2023-11-23 15:22:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const PORT = 3000;
 
 // Middlewares
 app.use(express.json()); // To parse the incoming requests with JSON payloads
@@ -9,5 +9,5 @@ app.use(express.urlencoded({extended:false}));
 // Routes
 app.use(require('./routes/index'));
 
-app.listen(3000);
-console.log('Server is running on port 3000');
+app.listen(PORT);
+console.log(`'Server is running on port: ${PORT}'`);
